dc8d7447dac4e69fe3d77407676b21004aa0a19f
dilandarren <dilandarren@gmail.com>
2025-02-06 22:26:04 +0100

    add Nothing Selected View

--- src/modules/journal/pages/JournalPage.jsx
+++ src/modules/journal/pages/JournalPage.jsx
@@ -1,15 +1,15 @@
-import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView } from "../views"
 
 export const JournalPage = () => {
     return (
         <JournalLayout>
 
-        <Typography>
+        {/* <Typography>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet itaque eligendi atque cumque ratione facilis sapiente enim sed mollitia quaerat animi unde aut accusamus, et maxime aliquam, officia veniam tempora.
-        </Typography>
+        </Typography> */}
 
-        {/* NothinSelected */}
+        <NothingSelectedView />
 
         </JournalLayout>
         
